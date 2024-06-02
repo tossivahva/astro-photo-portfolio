4987b36992ab9dd9719a3ada192d4429483666e8
tossivahva <tossivahva@gmail.com>
2024-06-02 22:30:06 +0300

    Finished with carousel animation

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,41 +1,69 @@
-import React, { useRef } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
+import { useWindowSize } from 'react-use';
 import {
     type ImagesType,
     images,
     randomImagesSet1,
     randomImagesSet2,
 } from '../utils/images.ts';
-import { useScroll, useTransform, motion } from 'framer-motion';
+import { useScroll, useTransform, motion, useMotionValueEvent } from 'framer-motion';
 
 const Carousel = () => {
+    const { width, height } = useWindowSize();
     const carouselWrapperRef = useRef<HTMLDivElement>(null);
     const { scrollYProgress } = useScroll({
         target: carouselWrapperRef,
         offset: ['start start', 'end start'],
     });
+    const maxScale = useMemo(() => {
+        const windowYRatio = height / width;
+        const xScale = 1.6;
+        const yScale = xScale * (16 / 9) * windowYRatio;
+        return Math.max(xScale, yScale);
+    }, [width, height]);
     
-    const bip = useTransform(scrollYProgress,
-        [0.3, 0.5, 0.66],
-        [3, 2.5, 1]);
+    const customScale = useTransform(scrollYProgress, [0.3, 0.5, 0.66], [maxScale * 1.1, maxScale, 1]);
+    
+    const sideCarouselItems = useTransform(scrollYProgress, [0.6, 0.66], [0, 1]);
+    const sideCarouselLeft = useTransform(scrollYProgress, [0.64, 0.66], [-30, 0]);
+    const sideCarouselRight = useTransform(scrollYProgress, [0.64, 0.66], [30, 0]);
+    
+    const [carouselVariant, setCarouselVariant] = useState<'active' | 'inactive'>('inactive');
+    useMotionValueEvent(scrollYProgress, 'change', (progress) => {
+        if (progress >= 0.67) {
+            setCarouselVariant('active');
+        } else {
+            setCarouselVariant('inactive');
+        }
+    });
     
     return (
-        <div className='bg-background flex flex-col gap-y-5 py-16'>
+        <motion.div
+            animate={carouselVariant}
+            className='bg-background flex flex-col gap-y-5 py-16'
+        >
             <div
                 ref={carouselWrapperRef}
                 className='overflow-clip mt-[-100vh] h-[300vh]'
             >
                 <div className='sticky top-0 h-screen flex items-center'>
                     <div className='flex w-full gap-5 left-1/2 -translate-x-1/2'>
-                        <div className='w-[66.66vw] shrink-0 rounded-2xl overflow-clip'>
+                        <motion.div
+                            style={{
+                                opacity: sideCarouselItems,
+                                x: sideCarouselLeft,
+                            }}
+                            className='w-[66.66vw] shrink-0 rounded-2xl overflow-clip'
+                        >
                             <img
                                 className='w-full h-full object-cover aspect-video'
                                 src={images[0].path}
                                 alt={images[0].name}
                             />
-                        </div>
+                        </motion.div>
                         <motion.div
-                            style={{ scale: bip }}
-                            className='w-[66.66vw] shrink-0 rounded-2xl overflow-clip'
+                            style={{ scale: customScale }}
+                            className='w-[66.66vw] shrink-0 rounded-2xl overflow-clip z-10'
                         >
                             <img
                                 className='w-full h-full object-cover aspect-video'
@@ -43,19 +71,34 @@ const Carousel = () => {
                                 alt={images[1].name}
                             />
                         </motion.div>
-                        <div className='w-[66.66vw] shrink-0 rounded-2xl overflow-clip'>
+                        <motion.div
+                            style={{
+                                opacity: sideCarouselItems,
+                                x: sideCarouselRight,
+                            }}
+                            className='w-[66.66vw] shrink-0 rounded-2xl overflow-clip'
+                        >
                             <img
                                 className='w-full h-full object-cover aspect-video'
                                 src={images[2].path}
                                 alt={images[2].name}
                             />
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
-            <SmallCarousel images={randomImagesSet1}/>
-            <SmallCarousel images={randomImagesSet2}/>
-        </div>
+            <motion.div
+                variants={{
+                    active: { opacity: 1, y: 0 },
+                    inactive: { opacity: 0, y: 50 },
+                }}
+                transition={{ duration: 0.5 }}
+                className='-mt-[calc((100vh-(370px*(16/9)))/2)] space-y-5'
+            >
+                <SmallCarousel images={randomImagesSet1}/>
+                <SmallCarousel images={randomImagesSet2}/>
+            </motion.div>
+        </motion.div>
     );
 };
 
